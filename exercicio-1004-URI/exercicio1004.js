184081f24df5debcb23abb9d045b0fb0a6218348
edumatheus65 <ematheusneves13@gmail.com>
2022-02-06 11:35:28 -0300

    Refatorando coding

--- exercicio-1004-URI/exercicio1004.js
+++ exercicio-1004-URI/exercicio1004.js
@@ -1,2 +1,10 @@
-const input = require('fs').readFileSync('stdin', 'utf8')
+var input = require('fs').readFileSync('stdin', 'utf8')
 
+    var valores = input.split('\n')
+
+var A = parseInt(valores.shift())
+var B = parseInt(valores.shift())
+
+var prod = A * B;
+
+console.log("PROD =", prod)
