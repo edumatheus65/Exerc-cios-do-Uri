7ca699f3892ed041811e27572995aa4a8f087da3
edumatheus65 <ematheusneves13@gmail.com>
2022-02-10 10:34:28 -0300

    problem 1010 solved

--- exercicio-1010-URI/exercicio1010.js
+++ exercicio-1010-URI/exercicio1010.js
@@ -2,18 +2,13 @@ var input = require("fs").readFileSync("stdin", "utf8")
 
 var linhas = input.split("\n")
 
-var = linhas.shift().split(" ")
-var infoItem2 = linhas.shift().split(" ")
-
-var idItem1 = parseInt(infoItem1.shift())
-var qtdItem1 = parseInt(infoItem1.shift())
-var valorUnitarioItem1 = parseFloat(infoItem1.shift())
-var valorTotalItem1 = valorUnitarioItem1 * qtdItem1
+var [idItem1, qtdItem1, valorUnitarioItem1] = linhas.shift().split(" ")
+var [idItem2, qtdItem2, valorUnitarioItem2] = linhas.shift().split(" ")
 
 
-var idItem2= parseInt(infoItem2.shift())
-var qtdItem2 = parseInt(infoItem2.shift())
-var valorUnitarioItem2 = parseFloat(infoItem2.shift())
+
+
+var valorTotalItem1 = valorUnitarioItem1 * qtdItem1 
 var valorTotalItem2 = valorUnitarioItem2 * qtdItem2
 
 var totalPagar = valorTotalItem1 + valorTotalItem2
