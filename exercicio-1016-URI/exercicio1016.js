a8a12d44cf18dd5c496d4f601fbb7a3f73570b0a
edumatheus65 <ematheusneves13@gmail.com>
2022-02-21 11:27:29 -0300

    Exercise 1016 solved

---
+++ exercicio-1016-URI/exercicio1016.js
@@ -0,0 +1,8 @@
+const input = require("fs").readFileSync("stdin", "utf8")
+
+// const qtdMinutos = (60 * input) / 30;
+
+// console.log(qtdMinutos + " minutos")
+
+console.log( input * 2) + " minutos"
+
