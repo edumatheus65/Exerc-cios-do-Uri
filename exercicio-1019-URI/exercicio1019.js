ab348fbdf16273376bebb7084931c355cbf121d7
edumatheus65 <ematheusneves13@gmail.com>
2022-02-24 07:20:32 -0300

    refatoing code

--- exercicio-1019-URI/exercicio1019.js
+++ exercicio-1019-URI/exercicio1019.js
@@ -12,6 +12,11 @@ for (let valor of valores) {
 
 console.log(resultado.join(":"))
 
+// Conceito : Ele vai pegar o 3600 vai armazenar dentro de valor,
+// e executar essa instrução
+// o push é um método para adicionar um elemento no fim do array
+// nesse caso como ele está vazio ele vai ser preenchido 3 vezes.
+
 // const qtdHoras = parseInt(qtdSegundos / 3600)
 // qtdSegundos = qtdSegundos % 3600
 
